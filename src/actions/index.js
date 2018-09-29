f53a3ce9cdec8400d8f5d9331855ba7d05f7fde5
AxosGT <chaosxaos@gmail.com>
2018-09-28 17:44:34 -0700

    added sumAdded to clickCount Actson

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 export const CLICK_COUNT = 'CLICK_COUNT';
 
 
-export function clickCount(){
+export function clickCount(sumAdded=1){
   return{
-    type:CLICK_COUNT
+    type:CLICK_COUNT,
+    sumAdded
   }
 }
--- src/reducers/clickCount.js
+++ src/reducers/clickCount.js
@@ -3,8 +3,7 @@ import {CLICK_COUNT} from '../actions/index';
 function clickCount(state = 0, action){
   switch (action.type) {
     case CLICK_COUNT:
-
-      return state+1
+      return state+action.sumAdded
     default:
       return state
   }
