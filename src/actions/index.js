c95de6b83c9d009ffaba83b47e383d63cfd46e67
AxosGT <chaosxaos@gmail.com>
2018-09-30 21:43:49 -0700

    saga boler plate

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
 export const CLICK_COUNT = 'CLICK_COUNT';
+export const USER_FETCH_REQUESTED = 'USER_FETCH_REQUESTED'
+export const SET_APP_JSON ='SET_APP_JSON'; //part of mySaga
 
 
 export function clickCount(sumAdded=0){
-  return{
-    type:CLICK_COUNT,
-    sumAdded
-  }
+  return{type:CLICK_COUNT,sumAdded}
 }
+
+export function userFetchRequested(){
+  return {type: USER_FETCH_REQUESTED}
+}
+
+// export function setAppJSON(setData){
+//   return {type: SET_APP_JSON, setData}
+// }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,20 @@
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { call, put, take } from 'redux-saga/effects'
+
+
+fetch('/game-info/gameInfo.json')
+
+function* fetchGameInfo() {
+   try {
+      const data = yield call(()=>fetch('/game-info/gameInfo.json'));
+      yield put({type: "SET_APP_JSON", setData:data.json()});
+   } catch (e) {
+      yield console.log(e.message)
+      //put({type: "USER_FETCH_FAILED", message: e.message});
+   }
+}
 
 function* mySaga() {
-  yield
+  yield take("USER_FETCH_REQUESTED", fetchGameInfo);
 }
 
 
